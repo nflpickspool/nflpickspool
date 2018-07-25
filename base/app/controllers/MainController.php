d7fe341ae113049d8f9530938460a9f63f59fab2
nflpickspool <brandoandpat@gmail.com>
2018-07-25 18:25:13 -0400

    Rename home template

--- base/app/controllers/MainController.php
+++ base/app/controllers/MainController.php
@@ -2,8 +2,7 @@
 class MainController extends Controller {
 
 	function render(){
-		//TODO: rename and fix homepage		
-		$this->f3->set('view','nav-top-fixed.htm');
+		$this->f3->set('view','home.htm');
         	$template=new Template;
         	echo $template->render('layout.htm');
 	}

a5bfbcfea07a8f2e186d038e1fbed8eb8500e92b
pmcnamara86 <pmcnamara86@gmail.com>
2018-08-07 16:11:40 -0400

    Add files via upload

---
+++ nfl_pp_site_layout_mvp_v1.sql
@@ -0,0 +1,53 @@
+landing page
+	1- "About NFL Picks Pool" hot button -> takes you to the rules page
+	2- "Sign Up" hot button -> takes you to sign up page -> form of attributes that are in users table
+	3- "Login" hot button in upper right hand corner that takes you to the login page 
+
+login page
+	1- enter username or email field
+	2- password field
+	3- forgot password button
+	
+post login page
+	1- My leagues button -> takes you to a page that lists the leagues you are in
+
+Tree
+	 View lines ->
+		View team win totals
+			Team Over Under Lines Table**1
+			Make My Picks! (or) View My Picks!**2
+			League Picks Table**3
+		View game lines
+			Week 1 Lines
+				Games W/Results**
+				Make My Picks! (or) View My Picks!**
+				League Picks and Results**
+			Week 2 Lines
+				Games w Results
+			Week 3 Lines
+			Week 4 Lines
+			
+	View Standings Page
+		Standings**5
+	View My Picks
+		View Game Picks
+			Brings up most relevant games form of unfilled picks**2
+			Shows query of all historical picks and results5
+		View Team Picks
+			Brings up O/U picks for individual**6
+	View Analytics
+		Pat to build eventually
+	
+
+
+**1 Team Over Under Lines TABLE
+	Fields
+		Team Name, Conference, Division, O/U #, Total Wins, Total Losses
+	Query
+	
+**2 Make My Picks
+	Fields	
+		Kickoff Datetime, Away Team Name, Home Team Name, Stadium, Network, Favorite Team, Point spread, money Line, o/u line, Spread pick, is_lock, money pick, o/u pick, suicide pick 
+	Query
+**3 Fields
+		

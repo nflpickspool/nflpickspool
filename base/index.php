6c50c602b9b74e06a16a373676847bba4f72872e
Kingmaker <admin@nflpickspool.com>
2018-08-11 22:38:39 -0400

    Shuffle around config files

--- base/index.php
+++ base/index.php
@@ -4,8 +4,8 @@ require_once("vendor/autoload.php");
 
 $f3 = Base::instance();
 
-$f3->config('config.ini');
-$f3->config('routes.ini');
+$f3->config('config/config.ini');
+$f3->config('config/routes.ini');
 
 new Session();
 

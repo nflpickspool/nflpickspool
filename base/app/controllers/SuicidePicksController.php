2b5bdda101e20e9313fb3b448b2a0bdf973f38cf
Kingmaker <admin@nflpickspool.com>
2018-09-01 22:20:35 -0400

    Add suicide picks form

--- base/app/controllers/SuicidePicksController.php
+++ base/app/controllers/SuicidePicksController.php
@@ -1,17 +1,75 @@
 <?php
 class SuicidePicksController extends UserController {
 
-	function afterroute() {
-		$template=new Template;
-        echo $template->render('homeLayout.htm');
-	}
+    function extractDataFromPost($suicidePicks){
+        $suicidePicks->league_year = $this->f3->get('POST.league_year');
+        $suicidePicks->league_week = $this->f3->get('POST.league_week');
+        $suicidePicks->user_id = $this->f3->get('POST.user_id');
+        $suicidePicks->game_id = explode('|',$this->f3->get('POST.pick'))[2];
+        $suicidePicks->suicide_pick = explode('|',$this->f3->get('POST.pick'))[0];
+    }
 	
 	function renderViewPicks(){
         $league_year = $this->f3->get('POST.league_year');
+        $this->f3->set('league_year',$league_year);
         $league_week = $this->f3->get('POST.league_week');
-        $this->f3->set('season',$season);
-        $this->f3->set('pageName',$season .'  Suicide Picks');
+        $this->f3->set('league_week',$league_week);        
+        $user_id=$this->f3->get('SESSION.user');
+        $this->f3->set('user_id',$user_id);
+        
+        $suicidePicks = new SuicidePicks($this->db);
+        $this->f3->set('previousPicks',$suicidePicks->getPreviousSuicidePicks($user_id,$league_year,$league_week));
+        //Check for existing pick for given week
+        $existingPick = $suicidePicks->getPickByIdYearAndWeek($user_id,$league_year,$league_week);
+        //Check if existing pick is closed
+        if(!empty($existingPick)){
+            $this->f3->set('existingPick',$existingPick[0]['suicide_pick']);
+            if(strtotime($existingPick[0]['kickoff_time']) < time()+(60-5)){
+                $closed = 1;
+                $this->f3->set('closed',$closed);
+            }
+        }
+        
+        //If not closed
+        if(!$closed){
+            $alive=$suicidePicks->checkIfAlive($user_id,$league_year);
+            //Only get list of available teams if still alive
+            if($alive[0]['alive']==0){
+                $this->f3->set('alive',0);
+            } else {
+                $this->f3->set('availableTeams',$suicidePicks->getAvailableTeamsForWeek($user_id,$league_year,$league_week));
+            }
+        }
+  
+        $this->f3->set('season',$league_year);
+        $this->f3->set('pageName',$league_year .'  Suicide Picks');
         $this->f3->set('view','suicidepicks.htm');	
 	}
+
+    function addSuicidePicks(){
+        $suicidePicks = new SuicidePicks($this->db);
+            //Make sure this pick hasn't been made before
+        $suicidePicks->load(array('league_year=? AND league_week=? AND user_id=?',$this->f3->get('POST.league_year'),$this->f3->get('POST.league_week'),$this->f3->get('POST.user_id')));
+        if($suicidePicks->dry()){
+            //Also check that the pick came in on time (kickoff - 5 minutes)
+            if(strtotime(explode('|',$this->f3->get('POST.pick'))[1]) > time()+(60*5)){
+                $this->extractDataFromPost($suicidePicks);
+                $suicidePicks->save();
+            }
+        }
+        $this->renderViewPicks();
+    }
+
+    function updateSuicidePicks(){
+        //$this->f3->set('view','post.htm');
+        //Check that the pick came in on time (kickoff - 5 minutes)
+        if(strtotime(explode('|',$this->f3->get('POST.pick'))[1]) > time()+(60*5)){
+            $suicidePicks = new SuicidePicks($this->db);
+            $suicidePicks->load(array('league_year=? AND league_week=? AND user_id=?',$this->f3->get('POST.league_year'),$this->f3->get('POST.league_week'),$this->f3->get('POST.user_id')));
+            $this->extractDataFromPost($suicidePicks);
+            $suicidePicks->update();
+        }
+        $this->renderViewPicks();
+    }
 }
 
---
+++ base/app/models/SuicidePicks.php
@@ -0,0 +1,134 @@
+<?php
+
+class SuicidePicks extends DB\SQL\Mapper{
+
+	public function __construct(DB\SQL $db) {
+	    parent::__construct($db,'suicide_picks');
+	}
+	
+	public function all() {
+	    $this->load();
+	    return $this->query;
+	}
+
+	public function getById($id) {
+	    $this->load(array('id=?',$id));
+	    return $this->query;
+	}
+
+    public function getPickByIdYearAndWeek($user_id,$league_year,$league_week) {
+		return $this->db->exec(
+            "SELECT sp.league_year, sp.league_week, g.kickoff_time, sp.suicide_pick ".
+            "FROM suicide_picks AS sp ".
+            "LEFT JOIN games as g ".
+            "ON game_id = g.id ".
+            "WHERE sp.league_year =  ".$league_year." ". 
+            "AND sp.league_week =  ".$league_week." ".
+            "AND sp.user_id = ".$user_id .";"
+        );
+	}
+
+
+    public function checkIfAlive($user_id,$league_year) {
+		return $this->db->exec(
+            "SELECT min(correct) ".
+            "AS alive ".
+            "FROM suicide_picks ".
+            "GROUP BY league_year, user_id ".
+            "HAVING league_year = ".$league_year ." ".
+            "AND ".$user_id.";"
+		);
+	}
+
+    public function getAvailableTeamsForWeek($user_id,$league_year,$league_week) {
+		return $this->db->exec(
+            "SELECT ".
+            "g.id as game_id, ".
+            "ta.id as team_id, ".
+            "ta.team as team, ".
+            "th.team as other_team, ".
+            "CONCAT(IF(ta.team=ft.team, '(-', '(+'),g.point_spread, ') @  ') AS point_spread, ".
+            "kickoff_time, ".
+            "u.id as user_id, ".
+            "sp.suicide_pick, ".
+            "sp.correct ".
+            "FROM games as g ".
+            "CROSS JOIN users AS u ".
+            "LEFT JOIN teams AS ta ". 
+            "  ON away = ta.id ".
+            "LEFT JOIN teams AS th ". 
+            "  ON home = th.id ".
+            "LEFT JOIN teams AS ft ".
+            "  ON favorite = ft.id ".
+            "LEFT JOIN suicide_picks AS sp ".
+            "  ON ta.id = sp.suicide_pick ".
+            "WHERE g.league_year =  ".$league_year." ". 
+            " AND g.league_week =  ".$league_week." ".
+            " AND u.id = ".$user_id." ".
+            " AND kickoff_time > '".date('Y-m-d H:i:s')."' ".
+            " AND correct IS NULL ".
+            "UNION ".
+            "SELECT ".
+            "g.id, ".
+            "th.id, ".
+            "th.team, ".
+            "ta.team, ".
+            "CONCAT(IF(th.team=ft.team, '(-', '(+'),g.point_spread,') vs.'), ".
+            "kickoff_time, ".
+            "u.id, ".
+            "sp.suicide_pick, ".
+            "sp.correct ".
+            "FROM games as g ".
+            "CROSS JOIN users AS u ".
+            "LEFT JOIN teams AS ta ". 
+            "  ON away = ta.id ".
+            "LEFT JOIN teams AS th ". 
+            "  ON home = th.id ".
+            "LEFT JOIN teams AS ft ".
+            "  ON favorite = ft.id ".
+            "LEFT JOIN suicide_picks AS sp ".
+            "  ON th.id = sp.suicide_pick ".
+            "WHERE g.league_year =  ".$league_year." ". 
+            " AND g.league_week =  ".$league_week." ".
+            " AND u.id = ".$user_id." ".
+            " AND kickoff_time > '".date('Y-m-d H:i:s')."' ".
+            " AND correct IS NULL".
+            " ORDER BY kickoff_time,team;"
+		);
+    }
+
+    public function getPreviousSuicidePicks($user_id,$league_year,$league_week) {
+        return $this->db->exec(
+            "SELECT ".
+            "sp.league_week as league_week, ".
+            "sp.id as game_id, ".
+            "t.team as team, ".
+            "u.id as user_id, ".
+            "sp.suicide_pick, ".
+            "sp.correct ".
+            "FROM suicide_picks as sp ".
+            "CROSS JOIN users AS u ".
+            "LEFT JOIN teams AS t ".
+            "ON sp.suicide_pick = t.id ".
+            "WHERE sp.league_year =  ".$league_year." ". 
+            " AND u.id = ".$user_id.
+            " ORDER BY league_week;"
+        );
+    }
+
+	public function add() {
+	    $this->copyFrom('POST');
+	    $this->save();
+	}
+	
+	public function edit($id) {
+	    $this->load(array('id=?',$id));
+	    $this->copyFrom('POST');
+	    $this->update();
+	}
+	
+	public function delete($id) {
+	    $this->load(array('id=?',$id));
+	    $this->erase();
+	}
+}
